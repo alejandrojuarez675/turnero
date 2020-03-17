18f121d8c62522389d6a3e97ce8f3c14e4b4b388
alejandro <alejandro.juarez@tecso.coop>
2020-03-17 19:38:27 -0300

    Fix routing

--- turnero-hospital-espaniol/src/app/app-routing.module.ts
+++ turnero-hospital-espaniol/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '**', loadChildren: './modules/home/home.module#HomeModule'}
+  {path: 'home', loadChildren: './modules/home/home.module#HomeModule'},
+  {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
--- turnero-hospital-espaniol/src/app/modules/home/home-routing.module.ts
+++ turnero-hospital-espaniol/src/app/modules/home/home-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: '**', component: DashboardComponent}
+  {path: '', component: DashboardComponent},
 ];
 
 @NgModule({
