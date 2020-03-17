0237e317fecac5255e42ccbd56324185b3fb6ee6
alejandro <alejandro.juarez@tecso.coop>
2020-03-17 20:14:32 -0300

    Add Store

--- turnero-hospital-espaniol/src/app/app.module.ts
+++ turnero-hospital-espaniol/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forRoot({
+      router: routerReducer,
+    }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   exports: [
     CoreModule,
