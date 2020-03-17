8e15d47fc729da55b77a076a2e24e2e8b08bdf19
alejandro <alejandro.juarez@tecso.coop>
2020-03-17 19:29:55 -0300

    Se agrega header y footer style

--- turnero-hospital-espaniol/src/app/app-routing.module.ts
+++ turnero-hospital-espaniol/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', loadChildren: './modules/home/home.module#HomeModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- turnero-hospital-espaniol/src/app/modules/home/home.module.ts
+++ turnero-hospital-espaniol/src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
