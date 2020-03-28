39ed4125d4e967128f457e7104508187cac40f44
Alejandro Juarez <alejandro.juarez@tecso.coop>
2020-03-28 17:21:18 -0300

    Add cambio de mes al scheduler

---
+++ turnero-hospital-espaniol/src/app/modules/home/components/scheduler/scheduler-utils.ts
@@ -0,0 +1,44 @@
+export const toMonthString = (month: number) => {
+    let result = '';
+
+    switch (month) {
+        case 0:
+            result = 'enero';
+            break;
+        case 1:
+            result = 'febrero';
+            break;
+        case 2:
+            result = 'marzo';
+            break;
+        case 3:
+            result = 'abril';
+            break;
+        case 4:
+            result = 'mayo';
+            break;
+        case 5:
+            result = 'junio';
+            break;
+        case 6:
+            result = 'julio';
+            break;
+        case 7:
+            result = 'agosto';
+            break;
+        case 8:
+            result = 'septiembre';
+            break;
+        case 9:
+            result = 'octubre';
+            break;
+        case 10:
+            result = 'noviembre';
+            break;
+        case 11:
+            result = 'diciembre';
+            break;
+    }
+    result = result.toUpperCase();
+    return result;
+};
--- turnero-hospital-espaniol/src/app/modules/home/components/scheduler/scheduler.component.ts
+++ turnero-hospital-espaniol/src/app/modules/home/components/scheduler/scheduler.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
 import { addDays, addHours, endOfDay, endOfMonth, isSameDay, isSameMonth, startOfDay, subDays } from 'date-fns';
-import { Subject } from 'rxjs';
+import { Subject, Scheduler, from } from 'rxjs';
+import { toMonthString } from './scheduler-utils';
 
 
 const colors: any = {
@@ -21,12 +22,14 @@ export class SchedulerComponent {
 
   CalendarView = CalendarView;
   viewDate: Date = new Date();
+  activeDayIsOpen: boolean;
+  view: CalendarView = CalendarView.Month;
 
   refresh: Subject<any> = new Subject();
 
   events: CalendarEvent[] = [
     {
-      start: startOfDay((new Date('2020-04-01'))),
+      start: endOfDay(new Date('2020-04-01')),
       title: 'Evento Ejemplo',
       color: colors.blue,
     },
@@ -38,4 +41,11 @@ export class SchedulerComponent {
     alert('click en ' + date);
   }
 
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+
+  _toMonthString(month: number) {
+    return toMonthString(month);
+  }
 }
