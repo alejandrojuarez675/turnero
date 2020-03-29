9f66d01924526d72dec0a0863fb211c42fca784c
Alejandro Juarez <alejandro.juarez@tecso.coop>
2020-03-29 15:12:12 -0300

    add bash deploy demo modo produccion

---
+++ deploy-prod-demo.sh
@@ -0,0 +1,16 @@
+now=$(date)
+echo borrando archivos anteriores...
+cd ../turnero-demo/
+rm -R assets
+cd ../turnero-hospital-espaniol/turnero-hospital-espaniol/
+echo compilando...
+ng build --prod --base-href turnero-demo
+echo compilacion completa
+cd dist/turnero-hospital-espaniol/
+mv * ../../../../turnero-demo/
+cd ../../../../turnero-demo/
+sed -i 's+../../../assets+./assets+g' main.js
+echo publicando demo en github
+git add .
+git commit -m "commit automatico - ${now}"
+git push
