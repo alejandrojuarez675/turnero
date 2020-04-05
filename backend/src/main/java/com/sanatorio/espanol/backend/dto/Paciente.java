c391ec4bb14b825eaf3a25da3772f225a7ef07f9
leandro <patricia.brumatti@tecso.coop>
2020-04-05 10:28:38 -0300

    set and get

--- backend/src/main/java/com/sanatorio/espanol/backend/dto/Paciente.java
+++ backend/src/main/java/com/sanatorio/espanol/backend/dto/Paciente.java
@@ -12,4 +12,53 @@ public class Paciente {
 	public Date fechaNacimiento; 
 	public Integer codigoObraSocial;
 	public Integer codigoPlan;
+	
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getNombreApellido() {
+		return nombreApellido;
+	}
+	public void setNombreApellido(String nombreApellido) {
+		this.nombreApellido = nombreApellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public Integer getCodigoObraSocial() {
+		return codigoObraSocial;
+	}
+	public void setCodigoObraSocial(Integer codigoObraSocial) {
+		this.codigoObraSocial = codigoObraSocial;
+	}
+	public Integer getCodigoPlan() {
+		return codigoPlan;
+	}
+	public void setCodigoPlan(Integer codigoPlan) {
+		this.codigoPlan = codigoPlan;
+	}
 }
